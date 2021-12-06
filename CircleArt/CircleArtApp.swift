bf8af5656740801d002262b668ae877162b7b470
jclegg03 <jjc2003@comcast.net>
2021-12-06 11:30:18 -0700

    made my circle art project

---
+++ CircleArt/CircleArtApp.swift
@@ -0,0 +1,20 @@
+//
+//  CircleArtApp.swift
+//  CircleArt
+//
+//  Created by Clegg, Jay on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CircleArtApp: App
+{
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            ContentView()
+        }
+    }
+}
